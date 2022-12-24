645e7f257c13830a59a77da22fbf7db49b3564c2
Sajed Shaikh <sajed.shaikh@hotmail.com>
2022-12-24 18:15:16 +0530

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Sajed Shaikh on 24/12/22.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
